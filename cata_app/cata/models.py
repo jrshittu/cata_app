3c50aa54f9b638677f670ac4cccdca10284fa61a
Abideen <misterjrshittu@gmail.com>
2023-08-03 21:03:24 +0100

    Started cata's db

--- cata_app/cata/models.py
+++ cata_app/cata/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+    publisher_name = models.CharField("Publisher's Name", max_length=200)
+
+def __str__(self):
+    return self.publisher_name
+
+class Publication(models.Model):
+    type_choices = [('NEWSPAPER', 'Newspaper'), ('MAGAZINE', 'Magazine')]
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, blank=True)
+    publication_name = models.CharField("Publication Name", max_length=200)
+    publication_notes = models.CharField("Publication Notes", max_length=200, blank=True)
+    publication_type = models.CharField("Publication Type", max_length=200, choices=type_choices)
+
