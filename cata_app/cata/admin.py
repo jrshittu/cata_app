beab01e75214549cbcc3fc13a7c45198f79686cd
Abideen <misterjrshittu@gmail.com>
2023-08-04 18:14:25 +0100

    Updated admin.py

--- cata_app/cata/admin.py
+++ cata_app/cata/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Publisher, Publication
+from .models import Publisher, Publication, Article, Author, Edition, Tag
 # Register your models here.
 
 admin.site.register(Publisher)
 admin.site.register(Publication)
+admin.site.register(Article)
+admin.site.register(Edition)
+admin.site.register(Tag)
+admin.site.register(Author)
