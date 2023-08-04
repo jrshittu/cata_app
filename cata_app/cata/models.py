f1ff7c4d1e2f47e335f89dfc3f9a7720fae3bb5b
Abideen <misterjrshittu@gmail.com>
2023-08-04 17:36:57 +0100

    updated models.py

--- cata_app/cata/models.py
+++ cata_app/cata/models.py
@@ -14,3 +14,18 @@ class Publication(models.Model):
     publication_notes = models.CharField("Publication Notes", max_length=200, blank=True)
     publication_type = models.CharField("Publication Type", max_length=200, choices=type_choices)
 
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    article_id = models.BigAutoField(primary_key=True)
+    keywords = models.TextField(default=0)
+class Author(models.Model):
+    author_name = models.CharField(max_length=200)
+    author_id = models.BigAutoField(primary_key=True)
+    
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=200)
+    tag_id = models.BigAutoField(primary_key=True)
+
+class Edition(models.Model):
+    edition_name = models.CharField(max_length=200)
+    edition_id = models.BigAutoField(primary_key=True)
